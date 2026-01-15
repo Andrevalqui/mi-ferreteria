d0813e11e1d2916979ed2d5e79a02e01d4fdecb5
Andrevalqui <luiggi.valqui@tecsup.edu.pe>
2026-01-14 20:57:45 -0500

    Update urls.py

--- mi_erp/urls.py
+++ mi_erp/urls.py
@@ -1,16 +1,18 @@
-# mi_erp/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-admin.site.site_header = "Administración | La Esquina del Shot"
-admin.site.site_title = "La Esquina del Shot"
-admin.site.index_title = "Administrador"
+# Configuración del Admin (Título)
+admin.site.site_header = "Administración | Ferretería Master"
+admin.site.site_title = "Ferretería Master"
+admin.site.index_title = "Panel de Control"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inventario/', include('inventario.urls')),
+    # CAMBIO IMPORTANTE: Quitamos 'inventario/' y dejamos comillas vacías ''
+    # Esto hace que la ferretería cargue en la página principal.
+    path('', include('inventario.urls')),
 ]
 
 if settings.DEBUG:
