cc6e8982c2aa638e3635e27f016c431976c16e97
Andrevalqui <luiggi.valqui@tecsup.edu.pe>
2026-01-14 21:53:42 -0500

    Update wsgi.py

--- mi_erp/wsgi.py
+++ mi_erp/wsgi.py
@@ -2,15 +2,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mi_erp.settings')
-
 application = get_wsgi_application()
-
-# --- TRUCO TEMPORAL PARA VERCEL ---
-from django.core.management import execute_from_command_line
-try:
-    execute_from_command_line(['manage.py', 'migrate', '--noinput'])
-except Exception as e:
-    print(f"Error en migraciones: {e}")
-# ----------------------------------
-
 app = application
