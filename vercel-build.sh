f8634224c422cc6a7f8dfb0bb4c9cf9468bb2423
Andrevalqui <luiggi.valqui@tecsup.edu.pe>
2026-01-16 16:30:13 -0500

    Update vercel-build.sh

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,5 +1,11 @@
-# vercel-build.sh corregido
+# vercel-build.sh
 python3 -m pip install -r requirements.txt
-python3 manage.py migrate --noinput
-python3 manage.py collectstatic --noinput
 
+# Generamos las migraciones por si acaso faltó algo
+python3 manage.py makemigrations --noinput
+
+# Ejecutamos las migraciones REALES (sin el --fake-initial)
+python3 manage.py migrate --noinput
+
+# Recolectamos archivos estáticos
+python3 manage.py collectstatic --noinput
