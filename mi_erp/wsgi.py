196070023c31270842b0a01dd8170226879dc404
Andrevalqui <luiggi.valqui@tecsup.edu.pe>
2026-01-16 16:20:48 -0500

    Update wsgi.py

--- mi_erp/wsgi.py
+++ mi_erp/wsgi.py
@@ -4,3 +4,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mi_erp.settings')
 
 application = get_wsgi_application()
+
+# AGREGA ESTA LÍNEA AL FINAL (Es el "traductor" para Vercel)
+app = application
